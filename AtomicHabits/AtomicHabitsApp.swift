2376aeebf23aab823111e96c4f7c98cf1de8f33b
Faustharus <chailloleau-damien@outlook.fr>
2023-12-06 20:37:42 +0100

    Initial Commit

---
+++ AtomicHabits/AtomicHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomicHabitsApp.swift
+//  AtomicHabits
+//
+//  Created by Damien Chailloleau on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AtomicHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
